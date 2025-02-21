c251a7e8bae439a699d082c92c5fb7aa4d6d94a9
hxb <839308443@qq.com>
2025-02-22 01:01:23 +0800

    feat: enhance System page with ProForm for settings management and improve layout

--- src/pages/Management/System.tsx
+++ src/pages/Management/System.tsx
@@ -1,20 +1,72 @@
-import { PageContainer } from '@ant-design/pro-components';
-import { Access, useAccess } from '@umijs/max';
-import { Button } from 'antd';
+import {
+  PageContainer,
+  ProForm,
+  ProFormText,
+  ProFormTextArea,
+} from '@ant-design/pro-components';
+import { message } from 'antd';
+
+type LayoutType = Parameters<typeof ProForm>[0]['layout'];
+
+const waitTime = (time: number = 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
 
 const SystemPage: React.FC = () => {
-  const access = useAccess();
   return (
     <PageContainer
-      style={{ height: '100%', overflow: 'auto' }}
+      style={{ height: '100%', overflow: 'auto', background: 'white' }}
       ghost
       header={{
-        title: '系统设置',
+        title: '基础设置',
       }}
     >
-      <Access accessible={access.canSeeAdmin}>
-        <Button>只有 Admin 可以看到这个按钮</Button>
-      </Access>
+      <ProForm<{
+        name: string;
+        company?: string;
+        useMode?: string;
+      }>
+        layout={'vertical'}
+        submitter={{
+          searchConfig: {
+            resetText: '取消',
+            submitText: '保存设置',
+          },
+          render: (props, doms) => {
+            return doms;
+          },
+        }}
+        onFinish={async (values) => {
+          await waitTime(2000);
+          console.log(values);
+          message.success('提交成功');
+        }}
+        params={{}}
+        request={async () => {
+          await waitTime(100);
+          return {
+            name: '智慧助手系统',
+            prompt: '你是一个卓越的 AI 助手，名叫：小智，善于帮助用户解决问题',
+          };
+        }}
+      >
+        <ProFormText
+          width="md"
+          name="name"
+          label="系统名称"
+          tooltip="最长为 24 位"
+          placeholder="请输入名称"
+        />
+        <ProFormTextArea
+          colProps={{ span: 24 }}
+          name="prompt"
+          label="新对话默认提示词"
+        />
+      </ProForm>
     </PageContainer>
   );
 };
