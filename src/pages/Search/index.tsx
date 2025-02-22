3eb7380a010870fdf359f124d4f8544a9addb3e8
hxb <839308443@qq.com>
2025-02-22 18:07:50 +0800

    feat: enhance Search component with multi-select and input fields

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,40 @@
-import { Segmented } from 'antd';
+import { Input, Segmented } from 'antd';
+import { useState } from 'react';
 import './index.css';
 
+import { SearchOutlined } from '@ant-design/icons';
+import type { SelectProps } from 'antd';
+import { Select, Space } from 'antd';
+
+const options: SelectProps['options'] = [];
+
+for (let i = 10; i < 36; i++) {
+  options.push({
+    label: i.toString(36) + i,
+    value: i.toString(36) + i,
+  });
+}
+
+const handleChange = (value: string[]) => {
+  console.log(`selected ${value}`);
+};
+
+const tagRender = (props: any) => {
+  return (
+    <span className="ml-4 text-[#4B5563] font-normal text-sm">
+      {props.label}
+    </span>
+  );
+};
+
 export default function Search() {
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+  const handleSelectChange = (value: string[]) => {
+    setSelectedItems(value);
+    handleChange(value);
+  };
+
   return (
     <div className="w-full h-full flex flex-col bg-white">
       <section className="tips-card p-8 flex flex-col items-center mt-20 w-[672px] mx-auto rounded-[20px]">
@@ -11,7 +44,7 @@ export default function Search() {
           <span className="text-[#4F46E5]">✨ 让知识触手可及</span>
         </p>
       </section>
-      <div className="mt-8 mx-auto">
+      <section className="mt-8 mx-auto">
         <Segmented<string>
           options={['语意检索', '关键词检索']}
           size="large"
@@ -19,7 +52,30 @@ export default function Search() {
             console.log(value); // string
           }}
         />
-      </div>
+      </section>
+      <section className="mt-8 mx-auto">
+        <Space style={{ width: '100%' }} direction="horizontal">
+          <Select
+            mode="multiple"
+            size="large"
+            allowClear
+            style={{ width: '192px' }}
+            placeholder="请选择"
+            value={selectedItems}
+            onChange={handleSelectChange}
+            options={options}
+            maxTagCount={0}
+            maxTagPlaceholder={() => `已选择 ${selectedItems.length} 个知识库`}
+            tagRender={tagRender}
+          />
+          <Input
+            className="w-[472px]"
+            size="large"
+            placeholder="请输入"
+            prefix={<SearchOutlined />}
+          />
+        </Space>
+      </section>
     </div>
   );
 }
