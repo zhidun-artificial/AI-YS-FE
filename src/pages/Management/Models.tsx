89ab2671522c400ca5dcd09e7cd181db2ff71876
tangkai <tangkai@megvii.com>
2025-02-23 15:51:26 +0800

    fix: 模型管理配置

--- src/pages/Management/Models.tsx
+++ src/pages/Management/Models.tsx
@@ -5,74 +5,102 @@ import {
   ProFormSlider,
   ProFormText,
 } from '@ant-design/pro-components';
-import { useAccess } from '@umijs/max';
+// import { useAccess } from '@umijs/max';
 import { message } from 'antd';
 
 const ModelPage: React.FC = () => {
-  const access = useAccess();
   return (
     <PageContainer
       style={{
         height: '100%',
         overflow: 'auto',
-        background: '#fff',
+        background: 'white',
         borderRadius: '12px',
       }}
       ghost
-      header={{
-        title: '模型管理',
-      }}
     >
-      {/* <Access accessible={access.canSeeAdmin}>
-        <Button>只有 Admin 可以看到这个按钮</Button>
-      </Access> */}
       <ProForm<{
-        name: string;
-        company: string;
+        user?: string;
+        api?: string;
+        model?: string;
       }>
-        onFinish={async (values) => {
+        layout={'vertical'}
+        submitter={{
+          searchConfig: {
+            submitText: '保存设置',
+          },
+          render: (props, doms) => {
+            return doms;
+          },
+          resetButtonProps: {
+            style: {
+              // 隐藏重置按钮
+              display: 'none',
+            },
+          },
+        }}
+
+        onFinish={(values) => {
           console.log(values);
           message.success('提交成功');
         }}
-        initialValues={{}}
+        params={{}}
+        request={async () => {
+          return {
+            user: 'Ollama API',
+            api: 'https://api.deepseek.com',
+            model: 'DeepSeek-R1:70B'
+          };
+        }}
       >
         <ProFormSelect
-          name="select"
+          options={[
+            {
+              value: 'Ollama API',
+              label: 'Ollama API',
+            },
+          ]}
+          width="xl"
+          name="user"
           label="模型提供方"
-          width={672}
-          valueEnum={{
-            open: '未解决',
-            closed: '已解决',
-          }}
-          placeholder="Please select a country"
-          rules={[{ required: true, message: 'Please select your country!' }]}
         />
         <ProFormText
+          width="xl"
           name="api"
-          width={672}
           label="API 域名"
-          placeholder="请输入名称"
+          placeholder="请输入API 域名"
         />
         <ProFormSelect
-          name="modal"
+          options={[
+            {
+              value: 'DeepSeek-R1:70B',
+              label: 'DeepSeek-R1:70B',
+            },
+          ]}
+          width="xl"
+          name="model"
           label="默认模型"
-          width={672}
-          valueEnum={{
-            open: '未解决',
-            closed: '已解决',
-          }}
-          placeholder="Please select a country"
-          rules={[{ required: true, message: 'Please select your country!' }]}
         />
-        <ProFormSlider width={672} name="context" label="上下文消息数量上限" />
+        <ProFormSlider filedConfig={{ ignoreWidth: false }}
+          min={0}
+          max={20}
+          marks={{
+            20: {
+              label: <span>20</span>
+            }
+          }}
+          step={1} width="md" name="max" label="上下文消息数量上限" />
         <ProFormSlider
-          name="slider"
-          width={672}
+          filedConfig={{ ignoreWidth: false }}
           min={0}
           max={1}
-          step={0.1}
-          label="严谨与想象（Temperature）"
-        />
+          marks={{
+            1: {
+              label: <span>1</span>
+            }
+          }}
+          step={0.1} width="md" name="switch" label="严谨与想象（Temperature）" />
+
       </ProForm>
     </PageContainer>
   );
