8e3cabd00d91fdf4b6ea84080112ca9e0e841bc1
null-studio <null-studio.tech@gmail.com>
2025-03-02 00:03:50 +0800

    feat: 优化文档预览功能，修复新窗口打开链接的问题

--- src/utils/preview.ts
+++ src/utils/preview.ts
@@ -2,10 +2,13 @@ import { stringify } from 'querystring'
 
 
 export const previewDocx = (file: { name: string, url: string }) => {
-  const query = stringify(file);
-  const url = `/preview/docx?${query}`;
-  const win = window.open(url, '_blank');
-  if (win?.document) win.document.title = file.name;
+  const query = stringify({ name: file.name, file: file.url });
+  const url = `${window.location.origin}${window.location.pathname}#/preview/docx?${query}`;
+  const win = window.open('', '_blank');
+  if (win) {
+    win.location.href = url;
+    if (win.document) win.document.title = file.name;
+  }
 }
 
 export const previewPdf = (file: { name: string, url: string }) => {
